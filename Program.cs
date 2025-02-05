c35d8a946bee1eae3de1321863125bef5df21889
Balaji <102133721+Balaji109@users.noreply.github.com>
2025-02-05 00:29:29 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Find_a_min_and_Max
+{
+class Program
+    {
+        static void Main()
+        {
+            int[] a = { 9, 2, 45, 32, 47, 23, 1 };
+            int Temp = a[0];
+            for (int i = 1; i < 7; i++)
+            {
+                if (Temp > a[i]) 
+                {
+                    Temp = a[i];
+                }
+                
+            }
+            Console.WriteLine("Min:" + Temp);
+            for (int i = 1; i < 7; i++)
+            {
+                if (Temp < a[i])
+                {
+                    Temp = a[i];
+                }
+
+            }
+            Console.WriteLine("Max:" + Temp);
+        }
+     }
+}
